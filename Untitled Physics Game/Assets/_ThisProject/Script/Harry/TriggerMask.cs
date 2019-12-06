c8bae1e0af5d4a21c73e753e46dd638e2959d7df
HarryharricChen <56270842+HarryharricChen@users.noreply.github.com>
2019-12-06 08:42:53 -0800

    Harry-Ball and Bridge Level

---
+++ Untitled Physics Game/Assets/_ThisProject/Script/Harry/TriggerMask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMask : MonoBehaviour
+{
+    public GameObject Mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mask.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        Mask.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Mask.SetActive(false);
+    }
+}

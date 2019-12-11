7e0abe675dffb732cfb37fe6bd774c82e1353ef2
HaoFan <fhbgdejia@hotmail.com>
2019-12-11 10:28:01 -0500

    FullLevel_Lack Arts & Final Level

---
+++ New Unity Project/Assets/EnterTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTrigger : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        player.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other != null)
+        {
+            Debug.Log("Something Enter" + this.gameObject.name);
+
+            if(other.tag == "Player")
+            {
+                other.transform.position = 
+            }
+
+        }
+    }
+
+}
---
+++ Untitled Physics Game/Assets/LookAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    Transform _playerTransform;
+
+    private void Start()
+    {
+        _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Vector2 playerPos = _playerTransform.position;
+        Vector2 lookAtDir = new Vector2(playerPos.x - this.transform.position.x, playerPos.y - this.transform.position.y);
+
+        this.transform.up = lookAtDir;
+    }
+}

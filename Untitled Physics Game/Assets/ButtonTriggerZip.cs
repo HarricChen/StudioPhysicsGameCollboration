18639919527430a94ca56f26110bc4819c141007
HaoFan <fhbgdejia@hotmail.com>
2019-12-12 16:23:24 -0500

    191212_FullLevel

---
+++ Untitled Physics Game/Assets/ButtonTriggerZip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTriggerZip : MonoBehaviour
+{
+
+    ZipMove zip;
+
+    private void Start()
+    {
+        zip = GameObject.Find("Zip").GetComponent<ZipMove>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other != null)
+        {
+            if (other.tag == "Player" || other.tag == "Hinge")
+            {
+                zip.isMove = true;
+            }
+        }
+    }
+
+}
---
+++ Untitled Physics Game/Assets/_ThisProject/Script/Hao/ZipMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZipMove : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _targetPos;
+
+    [SerializeField]
+    float _moveSpeed;
+
+    public bool isMove;
+
+
+    void Update()
+    {
+        if(isMove == true)
+        {
+            this.transform.position = Vector2.Lerp(this.transform.position, _targetPos.position, _moveSpeed * Time.deltaTime);
+        }
+    }
+}

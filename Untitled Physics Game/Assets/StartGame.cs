3fd5119d58a1f5b4871fd1875591f1b070dabd18
HaoFan <fhbgdejia@hotmail.com>
2019-12-17 19:05:03 -0500

    The Build submit for the class.

---
+++ Untitled Physics Game/Assets/StartGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public Color color;
+    public float startGameTime;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other != null)
+        {
+            if (other.tag == "Start")
+            {
+                Debug.Log("Start Enter");
+                sprite.color = new Color(color.r, color.g, color.b);
+                SceneManager.LoadScene(1);
+            }
+
+            if(other.tag == "Exit")
+            {
+                Debug.Log("Exit Enter");
+                sprite.color = new Color(color.r, color.g, color.b);
+                Application.Quit();
+            }
+
+        }
+    }
+
+}
